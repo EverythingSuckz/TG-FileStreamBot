3d0a9f613043b275a1a914cf27c5c8ddb9099fe3
Wrench <me@jyothis.gq>
2021-06-11 18:40:13 +0530

    Fix: unnoticed typos

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 @StreamBot.on_message(filters.private & (filters.document | filters.video | filters.audio), group=4)
 async def media_receive_handler(_, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-    stream_link = Var.URL + '/stream/' + str(log_msg.message_id)
+    stream_link = Var.URL + 'stream/' + str(log_msg.message_id)
     online_link = Var.URL + str(log_msg.message_id)
     await m.reply_text(
         text="`{}`".format(stream_link),
