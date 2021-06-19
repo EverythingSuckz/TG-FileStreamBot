98906b312d10e62de5a85c98aaf11bd9cd43fa8a
EverythingSuckz <JyothisJayanth05@gmail.com>
2021-06-19 22:02:30 +0530

    More UI improvements :sparkles:

---
+++ WebStreamer/utils/file_size.py
@@ -0,0 +1,3 @@
+def human_size(bytes, units=[' bytes','KB','MB','GB','TB', 'PB', 'EB']):
+    """ Returns a human readable string representation of bytes """
+    return str(bytes) + units[0] if int(bytes) < 1024 else human_size(int(bytes)>>10, units[1:])
--- WebStreamer/utils/render_template.py
+++ WebStreamer/utils/render_template.py
@@ -1,11 +1,13 @@
 from WebStreamer.vars import Var
 from WebStreamer.bot import StreamBot
 from WebStreamer.utils.custom_dl import TGCustomYield
+from WebStreamer.utils.file_size import human_size
 import urllib.parse
 import secrets
 import mimetypes
 import aiofiles
 import logging
+import aiohttp
 
 async def fetch_properties(message_id):
     media_msg = await StreamBot.get_messages(Var.BIN_CHANNEL, message_id)
@@ -20,16 +22,22 @@ async def fetch_properties(message_id):
 async def render_page(message_id):
     file_name, mime_type = await fetch_properties(message_id)
     src = urllib.parse.urljoin(Var.URL, str(message_id))
-    async with aiofiles.open('WebStreamer/template/req.html') as r:
-        audio_formats = ['audio/mpeg', 'audio/mp4', 'audio/x-mpegurl', 'audio/vnd.wav']
-        video_formats = ['video/mp4', 'video/avi', 'video/ogg', 'video/h264', 'video/h265', 'video/x-matroska']
-        if mime_type.lower() in video_formats:
+    audio_formats = ['audio/mpeg', 'audio/mp4', 'audio/x-mpegurl', 'audio/vnd.wav']
+    video_formats = ['video/mp4', 'video/avi', 'video/ogg', 'video/h264', 'video/h265', 'video/x-matroska']
+    if mime_type.lower() in video_formats:
+        async with aiofiles.open('WebStreamer/template/req.html') as r:
             heading = 'Watch {}'.format(file_name)
-        elif mime_type.lower() in audio_formats:
+            tag = mime_type.split('/')[0].strip()
+            html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
+    elif mime_type.lower() in audio_formats:
+        async with aiofiles.open('WebStreamer/template/req.html') as r:
             heading = 'Listen {}'.format(file_name)
-        else:
-            return None
-        tag = mime_type.split('/')[0].strip()
-        html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
-        logging.info(html)
-        return html
+            tag = mime_type.split('/')[0].strip()
+            html = (await r.read()).replace('tag', tag) % (heading, file_name, src)
+    else:
+        async with aiofiles.open('WebStreamer/template/dl.html') as r:
+            async with aiohttp.ClientSession() as s:
+                async with s.get(src) as u:
+                    file_size = human_size(u.headers.get('Content-Type'))
+                    html = (await r.read()) % (heading, file_name, src, file_size)
+    return html
