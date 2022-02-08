c313ddc530404bb7fc3f5f1dc22f83fa037d4444
wrench <me@jyothis.gq>
2022-02-08 22:30:23 +0530

    perf: faster start time

--- WebStreamer/bot/clients.py
+++ WebStreamer/bot/clients.py
@@ -1,6 +1,8 @@
 # This file is a part of TG-FileStreamBot
 # Coding : Jyothis Jayanth [@EverythingSuckz]
 
+import asyncio
+import logging
 from ..vars import Var
 from pyrogram import Client
 from WebStreamer.utils import TokenParser
@@ -14,22 +16,28 @@ async def initialize_clients():
     if not all_tokens:
         print("No additional clients found, using default client")
         return
-    for client_id, token in all_tokens.items():
-        instance = Client(
-            session_name=":memory:",
-            api_id=Var.API_ID,
-            api_hash=Var.API_HASH,
-            bot_token=token,
-            sleep_threshold=Var.SLEEP_THRESHOLD,
-            no_updates=True,
-        )
+    
+    async def start_client(client_id, token):
         try:
-            multi_clients[client_id] = await instance.start()
-        except Exception as e:
-            print(f"Failed starting Client - {client_id}; Error: {e}")
-            continue
-        work_loads[client_id] = 0
-        print(f"Started - Client {client_id}")
+            print(f"Starting - Client {client_id}")
+            if client_id == len(all_tokens):
+                await asyncio.sleep(2)
+                print("This will take some time, please wait...")
+            client = await Client(
+                session_name=":memory:",
+                api_id=Var.API_ID,
+                api_hash=Var.API_HASH,
+                bot_token=token,
+                sleep_threshold=Var.SLEEP_THRESHOLD,
+                no_updates=True,
+            ).start()
+            work_loads[client_id] = 0
+            return client_id, client
+        except Exception:
+            logging.error(f"Failed starting Client - {client_id} Error:", exc_info=True)
+    
+    clients = await asyncio.gather(*[start_client(i, token) for i, token in all_tokens.items()])
+    multi_clients.update(dict(clients))
     if len(multi_clients) != 1:
         Var.MULTI_CLIENT = True
         print("Multi-Client Mode Enabled")
