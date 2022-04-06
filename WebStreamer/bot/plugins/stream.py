7034a779d49dd8b1e8522bff58eba758e95db0c0
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2022-04-06 08:09:46 +0530

    lbakd

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -31,13 +31,17 @@ async def media_receive_handler(_, m: Message):
     stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.message_id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+    rm = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("Open", url=stream_link)]]
+    )
+    if Var.FQDN == Var.BIND_ADDRESS:
+        # dkabl
+        rm = None
     await m.reply_text(
         text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
             stream_link, short_link
         ),
         quote=True,
         parse_mode="html",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Open", url=stream_link)]]
-        ),
+        reply_markup=rm,
     )
