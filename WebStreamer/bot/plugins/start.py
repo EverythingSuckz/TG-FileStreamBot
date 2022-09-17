d31011618a46f22ede3579e3584de25cb1430ebc
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-17 23:37:11 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -9,5 +9,5 @@ from WebStreamer.bot import StreamBot
 @StreamBot.on_message(filters.command(["start", "help"]))
 async def start(_, m: Message):
     await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+        f'Hi {m.from_user.mention(style="md")}, Send me a file to get stream link.'
     )
