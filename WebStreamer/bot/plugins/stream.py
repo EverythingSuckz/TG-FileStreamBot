30c1e8b200ff7cb96e54e68f5d4fdd91fae06a19
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-18 01:31:32 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -6,7 +6,7 @@ from pyrogram import filters
 from WebStreamer.vars import Var
 from urllib.parse import quote_plus
 from WebStreamer.bot import StreamBot
-from WebStreamer.utils import get_hash, get_name
+from WebStreamer.utils import getFileid
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -27,24 +27,11 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     ),
     group=4,
 )
-async def media_receive_handler(_, m: Message):
-    log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-    new_msg = await m.forward(-1001731742687)
-    stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
-    str_link = f"{Var.URL}{new_msg.id}/{quote_plus(get_name(m))}?hash={get_hash(new_msg)}"
-    short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.id}"
-    logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
-    rm = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Open", url=stream_link)]]
-    )
-    if Var.FQDN == Var.BIND_ADDRESS:
-        # dkabl
-        rm = None
-    await m.reply_text(
-        text="<code>{}</code>\n(<a href='{}'>shortened</a>) {}".format(
-            stream_link, short_link, str_link
-        ),
-        quote=True,
-        parse_mode=ParseMode.HTML,
-        reply_markup=rm,
-    )
+async def getStreamlink(bot, message):
+    try:
+        await message.reply(
+            text=f"{Var.URL}/{getFileid(message)}",
+            quote=True
+        )
+    except Exception as e:
+        await message.reply(e)
