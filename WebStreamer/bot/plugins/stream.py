3b43a9a4b685453f3c207d1d8f61b0c03f316b4d
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-18 19:06:08 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -22,10 +22,23 @@ from WebStreamer.utils.file_properties import getNew, fileId
     group=4,
 )
 async def getStreamlink(bot, message):
+    
+    if message.photo:
+        await message.reply(
+            text="Don't send me photos, send them as document.",
+            quote=True
+        )
+        
     try:
         await message.reply(
             text=f"{Var.URL}{getNew(fileId(message))[0]}",
             quote=True
+        )
+        await client.copy_message(
+            chat_id=Var.BIN_CHANNEL,
+            from_chat_id=message.chat.id,
+            message_id=message.id,
+            caption='Hello'
         )
     except Exception as e:
         await message.reply(
