1839ba8f0b8f2b7ce0610419120772ecb7ea4a1a
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-18 12:09:25 +0530

    Update file_properties.py

--- WebStreamer/utils/file_properties.py
+++ WebStreamer/utils/file_properties.py
@@ -53,7 +53,7 @@ def get_name(media_msg: Message) -> str:
     media = get_media_from_message(media_msg)
     return getattr(media, 'file_name', "")
 
-def getFileid(message: "Message") -> Any:
+def fileId(message: "Message") -> Any:
     media_types = (
         "audio",
         "document",
@@ -68,3 +68,37 @@ def getFileid(message: "Message") -> Any:
         media = getattr(message, attr, None)
         if media:
             return media.file_id
+
+def encode_file_id(s: bytes) -> str:
+    r = b""
+    n = 0
+
+    for i in s + bytes([22]) + bytes([4]):
+        if i == 0:
+            n += 1
+        else:
+            if n:
+                r += b"\x00" + bytes([n])
+                n = 0
+
+            r += bytes([i])
+
+    return base64.urlsafe_b64encode(r).decode().rstrip("=")
+
+def encode_file_ref(file_ref: bytes) -> str:
+    return base64.urlsafe_b64encode(file_ref).decode().rstrip("=")
+
+def getNew(new_file_id):
+    """Return file_id, file_ref"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "<iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
