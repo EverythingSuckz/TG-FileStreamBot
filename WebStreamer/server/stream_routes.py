6868c9f023bd0b3df221860f6d10957b95fafd60
Mahesh <44301650+Mahesh0253@users.noreply.github.com>
2022-11-05 15:14:05 +0530

    Fix stream seeking

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -93,14 +93,24 @@ async def media_streamer(request: web.Request, message_id: int, secure_hash: str
         from_bytes = request.http_range.start or 0
         until_bytes = request.http_range.stop or file_size - 1
 
-    req_length = until_bytes - from_bytes
-    new_chunk_size = await utils.chunk_size(req_length)
-    offset = await utils.offset_fix(from_bytes, new_chunk_size)
+    if (until_bytes > file_size) or (from_bytes < 0) or (until_bytes < from_bytes):
+        return web.Response(
+            status=416,
+            body="416: Range not satisfiable",
+            headers={"Content-Range": f"bytes */{file_size}"},
+        )
+
+    chunk_size = 1024 * 1024
+    until_bytes = min(until_bytes, file_size - 1)
+
+    offset = from_bytes - (from_bytes % chunk_size)
     first_part_cut = from_bytes - offset
-    last_part_cut = (until_bytes % new_chunk_size) + 1
-    part_count = math.ceil(req_length / new_chunk_size)
+    last_part_cut = chunk_size - (until_bytes % chunk_size)
+
+    req_length = until_bytes - from_bytes
+    part_count = math.ceil(req_length / chunk_size)
     body = tg_connect.yield_file(
-        file_id, index, offset, first_part_cut, last_part_cut, part_count, new_chunk_size
+        file_id, index, offset, first_part_cut, last_part_cut, part_count, chunk_size
     )
 
     mime_type = file_id.mime_type
@@ -133,6 +143,6 @@ async def media_streamer(request: web.Request, message_id: int, secure_hash: str
     )
 
     if return_resp.status == 200:
-        return_resp.headers.add("Content-Length", str(file_size))
+        return_resp.headers.add("Content-Length", str(req_length + 1))
 
     return return_resp
