d929ecddffa5ec82ca6f05fe76be17bdfd3729ac
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-03-08 04:30:24 -0500

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -9,6 +9,7 @@ from WebStreamer.bot import StreamBot, logger
 from WebStreamer.utils import get_hash, get_name
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import FloodWait
 
 
 @StreamBot.on_message(
@@ -34,21 +35,74 @@ async def media_receive_handler(_, m: Message):
     short_link = f"{Var.URL}{file_hash}{log_msg.id}"
     logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
     try:
-        await m.reply_text(
-            text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
-                stream_link, short_link
-            ),
-            quote=True,
-            parse_mode=ParseMode.HTML,
+        if m.document:          
+           thumbnail_path = None
+        elif m.photo:
+           thumbnail_path = await StreamBot.download_media(m.photo.file_id)
+        elif m.video.thumbs:
+            thumbnail_path = await StreamBot.download_media(m.video.thumbs[0].file_id)
+
+        if thumbnail_path:
+            await StreamBot.send_photo(
+            chat_id=-1002144037144,
+            photo=thumbnail_path,
+            caption=stream_link,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Open", url=stream_link)]]))
+        else:
+               await StreamBot.send_message(
+               chat_id=-1002144037144,
+               text=stream_link,
+               reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Open", url=stream_link)]]))
+    except FloodWait as e:
+        print(f"Sleeping for {str(e.value)}s")
+        
+@StreamBot.on_message(
+    filters.channel
+    & ~filters.forwarded
+    & ~filters.media_group
+    & (
+            filters.document
+            | filters.video
+            | filters.video_note
+            | filters.audio
+            | filters.voice
+            | filters.photo
+    )
+)
+async def media_receive_handler(_, m: Message):
+    if m.chat.id == Var.BIN_CHANNEL:
+        return
+    if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS) ):
+        return await m.reply("You are not <b>allowed to use</b> this <a href='https://github.com/EverythingSuckz/TG-FileStreamBot'>bot</a>.", quote=True)
+    log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
+    file_hash = get_hash(log_msg, Var.HASH_LENGTH)
+    stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
+    short_link = f"{Var.URL}{file_hash}{log_msg.id}"
+    logger.info(f"Generated link: {stream_link}")
+    try:
+        if m.document:          
+           thumbnail_path = None
+        elif m.photo:
+           thumbnail_path = await StreamBot.download_media(m.photo.file_id)
+        elif m.video.thumbs:
+            thumbnail_path = await StreamBot.download_media(m.video.thumbs[0].file_id)
+        if thumbnail_path:
+          await StreamBot.send_photo(
+            chat_id=-1002144037144,
+            photo=thumbnail_path,
+            caption=stream_link,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open", url=stream_link)]]
-            ),
-        )
-    except errors.ButtonUrlInvalid:
-        await m.reply_text(
-            text="<code>{}</code>\n\nshortened: {})".format(
-                stream_link, short_link
-            ),
-            quote=True,
-            parse_mode=ParseMode.HTML,
-        )
+            ))
+        else:
+             await StreamBot.send_message(
+               chat_id=-1002144037144,
+               text=stream_link,
+               reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Open", url=stream_link)]]
+            ))
+    except FloodWait as e:
+        print(f"Sleeping for {str(e.value)}s")
+        
